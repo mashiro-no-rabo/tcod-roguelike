98e35fe0684b8ef713fa609c50fb09fcfd6e94bc
LOU Xun <aquarhead@ela.build>
2019-02-20 23:27:37 +0000

    Add movement and keyboard handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
 extern crate tcod;
 
-use tcod::console::*;
 use tcod::colors;
+use tcod::console::*;
 
 const SCREEN_WIDTH: i32 = 80;
 const SCREEN_HEIGHT: i32 = 50;
@@ -17,11 +17,38 @@ fn main() {
 
     tcod::system::set_fps(LIMIT_FPS);
 
+    let mut player_x = SCREEN_WIDTH / 2;
+    let mut player_y = SCREEN_HEIGHT / 2;
+
     while !root.window_closed() {
         root.set_default_foreground(colors::WHITE);
-        root.clear();
-        root.put_char(1, 1, '@', BackgroundFlag::None);
+        root.put_char(player_x, player_y, '@', BackgroundFlag::None);
         root.flush();
-        root.wait_for_keypress(true);
+
+        root.put_char(player_x, player_y, ' ', BackgroundFlag::None);
+        let exit = handle_keys(&mut root, &mut player_x, &mut player_y);
+        if exit {
+            break;
+        }
     }
+}
+
+fn handle_keys(root: &mut Root, player_x: &mut i32, player_y: &mut i32) -> bool {
+    use tcod::input::Key;
+    use tcod::input::KeyCode::*;
+
+    let key = root.wait_for_keypress(true);
+
+    match key {
+        // movement keys
+        Key { code: Up, .. } => *player_y -= 1,
+        Key { code: Down, .. } => *player_y += 1,
+        Key { code: Left, .. } => *player_x -= 1,
+        Key { code: Right, .. } => *player_x += 1,
+
+        Key { code: Escape, .. } => return true, // exit game
+
+        _ => {}
+    }
+    false
 }
